d24bf3ffd93a17ea919a949a37e12c384ddda122
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2019-05-24 14:27:46 -0400

    Output Cloudfront Distribution ID

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "cloudfront_domain_name" {
     value = "${aws_cloudfront_distribution.website_distribution.domain_name}"
 }
 
+output "cloudfront_distribution_id" {
+    value = "${aws_cloudfront_distribution.website_distribution.id}"
+}
+
 output "cloudfront_domain_hosted_zone_id" {
     value = "${aws_cloudfront_distribution.website_distribution.hosted_zone_id}"
 }
