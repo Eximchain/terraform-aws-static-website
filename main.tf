ece485226538b336d253a6ece1e163680c03346b
John O'Sullivan <john@eximchain.com>
2019-06-05 15:03:06 -0400

    Ensured that specific paths (i.e. site.com/page) will still return index.html, rather than a 404 for page.html

--- main.tf
+++ main.tf
@@ -78,6 +78,17 @@ resource "aws_s3_bucket" "website_content" {
     website {
         index_document = "index.html"
         error_document = "index.html"
+
+        routing_rules = <<EOF
+[{
+    "Condition": {
+        "HttpErrorCodeReturnedEquals": "404"
+    },
+    "Redirect": {
+        "ReplaceKeyWith": "index.html"
+    }
+}]
+EOF
     }
 
     logging {
