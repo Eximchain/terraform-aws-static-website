c82763b720258e51d9dd39aa20583eac149db0ff
Louis <loulamia@Louiss-Eximchain-MacBook-Pro.local>
2019-07-03 10:37:33 -0700

    Convert to Terraform 0.12 syntax

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,20 @@
 output "cloudfront_domain_name" {
-    value = "${aws_cloudfront_distribution.website_distribution.domain_name}"
+  value = aws_cloudfront_distribution.website_distribution.domain_name
 }
 
 output "cloudfront_distribution_id" {
-    value = "${aws_cloudfront_distribution.website_distribution.id}"
+  value = aws_cloudfront_distribution.website_distribution.id
 }
 
 output "cloudfront_domain_hosted_zone_id" {
-    value = "${aws_cloudfront_distribution.website_distribution.hosted_zone_id}"
+  value = aws_cloudfront_distribution.website_distribution.hosted_zone_id
 }
 
 output "website_dns" {
-    value = "${aws_route53_record.website.fqdn}"
+  value = aws_route53_record.website.fqdn
 }
 
 output "redirect_dns" {
-    value = "${element(coalescelist(aws_route53_record.redirect.*.fqdn, list("")), 0)}"
+  value = element(coalescelist(aws_route53_record.redirect.*.fqdn, [""]), 0)
 }
+
--- variables.tf
+++ variables.tf
@@ -2,78 +2,78 @@
 # REQUIRED VARIABLES
 # --------------------------------------------------------
 variable "dns_name" {
-    description = "The DNS name the website will be given"
+  description = "The DNS name the website will be given"
 }
 
 variable "domain_root" {
-    description = "The root domain hosting this, which must correspond to a public Hosted Zone name"
+  description = "The root domain hosting this, which must correspond to a public Hosted Zone name"
 }
 
 variable "website_bucket_name" {
-    description = "Name of the bucket in which to host the static website"
+  description = "Name of the bucket in which to host the static website"
 }
 
 variable "log_bucket_name" {
-    description = "Name of the bucket to hold logs for the website"
+  description = "Name of the bucket to hold logs for the website"
 }
 
 variable "github_website_repo" {
-    description = "Name of the repo with the static website on it"
+  description = "Name of the repo with the static website on it"
 }
 
 variable "github_website_branch" {
-    description = "Name of the branch with the static website on it"
+  description = "Name of the branch with the static website on it"
 }
 
 # --------------------------------------------------------
 # OPTIONAL VARIABLES
 # --------------------------------------------------------
 variable "aws_region" {
-    description = "AWS Region to use"
-    default     = "us-east-1"
+  description = "AWS Region to use"
+  default     = "us-east-1"
 }
 
 variable "force_destroy_buckets" {
-    description = "Set to true to force destroy buckets on terraform destroy"
-    default     = false
+  description = "Set to true to force destroy buckets on terraform destroy"
+  default     = false
 }
 
 variable "redirect_dns_name" {
-    description = "The DNS name to redirect to the website"
-    default     = ""
+  description = "The DNS name to redirect to the website"
+  default     = ""
 }
 
 variable "redirect_bucket_name" {
-    description = "Name of the bucket to host the redirect"
-    default     = ""
+  description = "Name of the bucket to host the redirect"
+  default     = ""
 }
 
 variable "github_owner" {
-    description = "Owner the repository with the static website belongs to"
-    default     = "Eximchain"
+  description = "Owner the repository with the static website belongs to"
+  default     = "Eximchain"
 }
 
 variable "acm_cert_domain" {
-    description = "The Domain of an ACM certificate that is valid for all domains the site will be hosted at. Will provision one if not provided."
-    default     = ""
+  description = "The Domain of an ACM certificate that is valid for all domains the site will be hosted at. Will provision one if not provided."
+  default     = ""
 }
 
 variable "acm_cert_arn" {
-    description = "The ARN of an ACM certificate that is valid for all domains the site will be hosted at. Will provision one if not provided."
-    default     = ""
+  description = "The ARN of an ACM certificate that is valid for all domains the site will be hosted at. Will provision one if not provided."
+  default     = ""
 }
 
 variable "pretty_project_description" {
-    description = "A pretty, human-readable name for the project to be used in comments"
-    default     = "static website"
+  description = "A pretty, human-readable name for the project to be used in comments"
+  default     = "static website"
 }
 
 variable "deployment_directory" {
-    description = "The directory in the repository in which the artifacts to deploy can be found"
-    default     = "./"
+  description = "The directory in the repository in which the artifacts to deploy can be found"
+  default     = "./"
 }
 
 variable "build_command" {
-    description = "The command to use to build the Website, if you want the pipeline to build it (e.g. 'npm run build').  If not specified, the pipeline will assume the static bundle is already built."
-    default     = ":"
+  description = "The command to use to build the Website, if you want the pipeline to build it (e.g. 'npm run build').  If not specified, the pipeline will assume the static bundle is already built."
+  default     = ":"
 }
